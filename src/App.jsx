915f7f13479d0c9809d8013536222204e76ba995
jisshub <jissmon476@gmail.com>
2022-11-17 19:27:53 +0530

    Commit New

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Video from './Video'
 import './App.css'
 
-const API_URL =
-	'https://raw.githubusercontent.com/codedamn-classrooms/tiktok-react-material/main/data.json'
+const API_URL = './data/tiktok.json'
 
 export default function App() {
 	const [videos, setVideos] = useState([])
